75e3e62230aa178ca52f3bb758b5b3c64cbd771f
erand2015 <erand2015@hotmail.com>
2026-03-08 02:27:59 +0100

    Versioni final i UI

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lex.app', // Mund ta lësh siç është ose ta ndryshosh
+  appName: 'Lex Associates', // Emri që do shfaqet në telefon
+  webDir: 'out' // KJO DUHET TË JETË 'out'
+};
+
+export default config;
